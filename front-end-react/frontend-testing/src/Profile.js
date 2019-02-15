5ec16b4862e9794e28c1155f9243ddb4ad898fa6
PrakharGoenka <prakhar.goinka@gmail.com>
2019-02-15 15:17:08 +0530

    frontend: Add profile page.

--- front-end-react/frontend-testing/src/Profile.js
+++ front-end-react/frontend-testing/src/Profile.js
@@ -1,9 +1,35 @@
 import React, {Component} from "react"
-
+import Test from "./Test"
 class Profile extends Component {
+
+	state = {
+		data: []
+	}
+	componentDidMount() {
+		fetch("https://reqres.in/api/users?page=2")
+			.then((res) => res.json())
+			.then(result => {
+				this.setState({
+					data: result.data,
+				})
+			},
+			(error) => {
+				this.setState({
+					placeholder: "error"
+				})
+			})		
+		}
+		handleChange = () => {this.props.history.push(`/`)}
 	render() {
+		const list = this.state.data.map((item,index) => <Test key={item.id} first_name={item.first_name} last_name={item.last_name}/>)
 		return (
+			<div>
 			<h3 className="text-prime text-center"> Welcome!!</h3>
+			<button onClick={this.handleChange}>Signout</button>
+			<div className="well well-sm text-prime text-center">
+			{list}
+			</div>
+			</div>
 		)
 	}
 }
--- front-end-react/frontend-testing/src/Test.js
+++ front-end-react/frontend-testing/src/Test.js
@@ -4,18 +4,9 @@ class Test extends Component {
 	render() {
 		return (
 			<Fragment>
-				<h1 className="text-danger text-center">Login</h1>
-				<form>
-				  <label>
-				    Username:
-				    <input type="text" name="Username" />
-				  </label>
-			   	  <label>
-				    Password:
-				    <input type="text" name="Password" />
-				  </label>
-				  	<input type="submit" value="Submit" />
-				</form>
+				<h2> First name: {this.props.first_name} </h2>
+				<h3>Last Name: {this.props.last_name}</h3>
+				<br/>
 			</Fragment>
 		)
 	}
