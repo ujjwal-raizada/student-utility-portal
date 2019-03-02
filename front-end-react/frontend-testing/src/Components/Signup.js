f89a4c4d66c3ef9c26580d94c9f1bd64a134b2d0
PrakharGoenka <prakhar.goinka@gmail.com>
2019-03-02 19:55:36 +0530

    frontend: Style Signup and Admin Page.

--- front-end-react/frontend-testing/src/Components/Signup.js
+++ front-end-react/frontend-testing/src/Components/Signup.js
@@ -57,72 +57,79 @@ class Signup extends Component {
 		return (
 			<Fragment>
 				<Header page="Signup" />
-				<div style = {{marginTop: '60px'}}>
-					<h1 >Signup</h1>
-					<p className="text-danger">{this.state.placeholder}</p>
-					<form>
+					<div className="text-center" style = {{marginTop: '60px'}}>
+						<h1 >Signup</h1>
+						<p className="text-danger">{this.state.placeholder}</p>
 
-						<div>
-							<label>
-								Username:  
-								<input 
-									type="text" 
-									name="username" 
-									value={this.state.username}
-									onChange={this.handleChange}
-								/>
-							</label>
-							<br/>
-							<br/>
-						</div>
+						<form>
 
-						<div>
-							<label>
-								Password:  
-								<input 
-									type="password" 
-									name="password" 
-									value={this.state.password} 
-									onChange={this.handleChange}
-								/>
-							</label>
-							<br/>
-							<br/>
-						</div>
-
-						<div>
-							<label>
-								User Type:
-								<select 
-									value={this.state.type}
-                  onChange={this.handleChange}
-                  name="type" 
-                	>
-									<option value="normal">Student</option>
-									<option value="admin">Admin</option>
-								</select>
-							</label>
-							<br/>
-							<br/>
-						</div>	
-
-						<div>
-							<label>
-								Description:<br/>
-								<textarea name="Description" />
+							<div>
+								<label>
+									Username:  
+									<input 
+										className = "well well-sm"
+										type="text" 
+										name="username" 
+										value={this.state.username}
+										onChange={this.handleChange}
+									/>
+								</label>
 								<br/>
-							</label>
-							<br/>
-						</div>											
+								<br/>
+							</div>
 
-						<div>
-							<input 
-								type="submit" 
-								name="Submit" 
-								onClick={this.handleSubmit}/>
-							<br/>							
-						</div>
-					</form>
+							<div>
+								<label>
+									Password:  
+									<input 
+										className = "well well-sm"
+										type="password" 
+										name="password" 
+										value={this.state.password} 
+										onChange={this.handleChange}
+									/>
+								</label>
+								<br/>
+								<br/>
+							</div>
+
+							<div >
+								<label >
+									User Type:
+									<select 
+										value={this.state.type}
+	                  onChange={this.handleChange}
+	                  name="type" 
+	                	>
+										<option value="normal">Student</option>
+										<option value="admin">Admin</option>
+									</select>
+								</label>
+								<br/>
+								<br/>
+							</div>	
+
+							<div>
+								<label>
+									Description:<br/>
+									<textarea 
+										className = "well well-sm"
+										style={{height: `100px` , width: `300px`}}
+										name="Description" />
+									<br/>
+								</label>
+								<br/>
+							</div>											
+
+							<div>
+								<input
+									className = "btn btn-primary" 
+									type="submit" 
+									name="Submit" 
+									onClick={this.handleSubmit}/>
+								<br/>							
+							</div>
+						</form>
 				</div>	
 			</Fragment>				
  		)
