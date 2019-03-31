e285a66505e1c31845946e85d6b88448456f92bb
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-04-01 02:08:07 +0530

    express-server: Deprecated Node-server, now express-server is the official server.

--- express-server/express-api-server/routes/index.js
+++ express-server/express-api-server/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 //LOGIN ROUTES//
 
 router.get('/', function a(req, res) {
-    res.send('Welcome to Student Util Portal. This is Backend API');
+    res.send('Welcome to Student Util Portal. This is the Backend API');
 });
 
 module.exports = router;
