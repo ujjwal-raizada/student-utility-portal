68b64a2a0e9e287992f8f691f5e00f71032a4f1c
Ujjwal Raizada <ujjwalraizada@gmail.com>
2019-04-01 03:53:38 +0530

    express-server: Add controller for notice creation and fetching.

---
+++ express-server/express-api-server/controllers/notice/noticeController.js
@@ -0,0 +1,98 @@
+var Notice = require('../../models/Notice');
+var Student = require('../../models/Student');
+var OfficialSource = require('../../models/OfficialSource');
+var async = require('async');
+
+// GET endpoint
+exports.get_all_notices = function(req, res) {
+
+    console.log('Fetching Notices...');
+    
+    async.parallel({
+        'Notice': function(callback) {
+            Notice.find()
+            .exec(callback);
+            console.log('Notices fetched.')
+        },
+
+    
+    }, function(err, result) {
+        if(err) throw err;
+        
+        console.log(result.Notice)
+        res.json(result.Notice)
+
+    });
+}
+
+exports.create_notice = function(req, res) {
+
+    title = req.body.title
+    body = req.body.body
+    eventDateTime = req.body.eventDateTime
+    source = req.body.username
+    username = req.body.username
+    tags = req.body.tags
+
+    res_data = {}
+    res_data['username'] = username
+    res_data['title'] = title
+    
+    async.parallel({
+
+
+        'OfficialSource': function(callback) {
+            OfficialSource.findOne({'username' : username})
+            .exec(callback);
+        },
+
+    }, function(err, result) {
+
+        if(err) {
+            res_data['message'] = 'Some error occured'
+            res_data['status'] = 'failure'
+            console.log('Some error occured')
+            res.json(res_data)
+        }
+
+        if (result.OfficialSource != null) {
+
+            notice_data = {}
+            notice_data.title = title
+            notice_data.body = body
+            notice_data.source = source
+            notice_data.tags = tags
+
+            if (eventDateTime == null)
+                notice_data.isEvent = false
+            else
+                notice_data.isEvent = true
+            
+            var new_notice = new Notice(notice_data)
+            new_notice.save(function(err, instance) {
+                if(err) {
+                    res_data['message'] = 'Some error occured'
+                    res_data['status'] = 'failure'
+                    console.log('Some error occured')
+                    res.json(res_data)
+                }
+                else {
+                    res_data['message'] = 'Notice created successfully'
+                    res_data['status'] = 'success'
+                    console.log('Notice created successfully!')
+                    console.log(new_notice)
+                    res_data['url'] = new_notice.url
+                    res.json(res_data)
+                }
+            });
+
+        }
+        else {
+            res_data['status'] = 'failure'
+            res_data['message'] = 'user not authorized to create notice.'
+            res.json(res_data)
+        }
+
+    });
+
+}
