7d4e1da97e9b8482da71b810d9616281793ae6fa
Pranjal Gupta <pranjalgupta2199@gmail.com>
2019-04-01 13:10:41 +0530

    express-routes: Add /students and /sources routes in admin

--- express-server/express-api-server/routes/admin.js
+++ express-server/express-api-server/routes/admin.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var path = '../controllers/admin/';
+var studentsController = require(path + '/studentsController');
+var sourcesController = require(path + '/sourcesController');
+
 router.post('/signup', function(req, res, next){
-    res.send("signup functionality for admin");
+    res.send("Signup functionality for admin");
 });
 router.post('/login', function(req, res, next){
     res.send("Login functionality for admin");
 });
-router.get('/students', function(req, res, next){
-    res.send("Display all the students");
-});
-router.get('/sources', function(req, res, next){
-    res.send("Display all the Official Sources");
-});
+router.get('/students', studentsController);
+router.get('/sources', sourcesController);
 module.exports = router;
