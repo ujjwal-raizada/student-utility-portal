431359f43d612f3b6b58feaf4b3232ec08d9db58
Pranjal Gupta <pranjalgupta2199@gmail.com>
2019-04-01 13:31:56 +0530

    express-routes: Add /login route for admin

--- express-server/express-api-server/routes/admin.js
+++ express-server/express-api-server/routes/admin.js
@@ -5,11 +5,10 @@ var path = '../controllers/admin/';
 var studentsController = require(path + '/studentsController');
 var sourcesController = require(path + '/sourcesController');
 var signupController = require(path + '/signupController');
+var loginController = require(path + '/loginController');
 
 router.post('/signup', signupController);
-router.post('/login', function(req, res, next){
-    res.send("Login functionality for admin");
-});
+router.post('/login', loginController);
 router.get('/students', studentsController);
 router.get('/sources', sourcesController);
 module.exports = router;
