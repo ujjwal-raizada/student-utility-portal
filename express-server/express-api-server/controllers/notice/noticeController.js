afc1e69f540b2adccb3dd9c195c11e3391f2e7f2
Pranjal Gupta <pranjalgupta2199@gmail.com>
2019-04-05 16:54:15 +0530

    express-routes: Add /star in /notice routes

--- express-server/express-api-server/controllers/notice/noticeController.js
+++ express-server/express-api-server/controllers/notice/noticeController.js
@@ -53,7 +53,7 @@ exports.get_all_notices = function(req, res) {
         res.json(notice_data)
 
     });
-}
+};
 
 exports.create_notice = function(req, res) {
 
@@ -69,8 +69,6 @@ exports.create_notice = function(req, res) {
     res_data['title'] = title
     
     async.parallel({
-
-
         'OfficialSource': function(callback) {
             OfficialSource.findOne({'username' : username})
             .exec(callback);
@@ -86,6 +84,8 @@ exports.create_notice = function(req, res) {
         }
 
         if (result.OfficialSource != null) {
+
+            noticeList = result.OfficialSource.noticeList;
 
             notice_data = {}
             notice_data.title = title
@@ -113,7 +113,12 @@ exports.create_notice = function(req, res) {
                     console.log(new_notice)
                     res_data['url'] = new_notice.url
                     res.json(res_data)
+
+                    // add noticeID to source's notice list
+                    noticeList.push(new_notice._id);
                 }
+            result.OfficialSource.updateOne({'noticeList' : noticeList});
+            result.OfficialSource.save();
             });
 
         }
@@ -125,7 +130,7 @@ exports.create_notice = function(req, res) {
 
     });
 
-}
+};
 
 exports.get_notice = function(req, res) {
 
@@ -202,7 +207,7 @@ exports.subscribe = function(req, res){
                 }
                 else {
                     res_data['status'] = 'failure';
-                    res_data['message'] = 'User has already subscribed to the Source';                    
+                    res_data['message'] = 'User has already subscribed to the Source';
                 }
             }
             else {
@@ -222,4 +227,99 @@ exports.subscribe = function(req, res){
         res.json(res_data);
         console.log(res_data);
     });
-}
+};
+
+exports.star = function(req, res){
+    var username = req.body.username;
+    var noticeSource = req.body.source;
+    var noticeID = req.body.ID;
+    var res_data = {
+        'username' : username,
+        'status' : '',
+        'message' : '',
+    }
+    async.parallel({
+        'Student' : function(callback){
+            Student.findOne({'username' : username})
+            .exec(callback);
+        },
+        'OfficialSource' : function(callback){
+            OfficialSource.findOne({'username' : username})
+            .exec(callback);
+        },
+        'NoticeCheck' : function(callback){
+            OfficialSource.findOne({'username' : noticeSource}, 'noticeList')
+            .exec(callback);
+        },
+
+    }, function(err, result){
+        if (err) throw err;
+
+        if (result.NoticeCheck != null){
+            console.log(`${username} starring a notice of ${noticeSource} with noticeID ${noticeID}`);
+
+            var temp_notice = result.NoticeCheck.noticeList;
+            const get_existingNotice = temp_notice.find(element => element == noticeID);
+
+            if (typeof get_existingNotice != 'undefined'){
+
+                res_data['status'] = 'success';
+                res_data['message'] = 'User starred the notice successfully';
+
+                var temp_sub = [];
+                if (result.Student != null) {
+                    // update the student doc
+                    temp_sub = result.Student.starNotice;
+                    const get_existingID = temp_sub.find(element => element == noticeID);
+                    if (!get_existingID) {
+                        temp_sub.push(noticeID);
+                        result.Student.updateOne({'starNotice' : temp_sub});
+                        result.Student.save();
+                    }
+                    else {
+                        res_data['status'] = 'failure';
+                        res_data['message'] = 'User has already starred the notice';
+                    }
+
+                }
+                else if (result.OfficialSource != null){
+                    // update the official source doc
+                    temp_sub = result.OfficialSource.starNotice;
+                    const get_existingID = temp_sub.find(element => element == noticeID);
+
+                    if (!get_existingID ) {
+                        temp_sub.push(noticeID);
+                        result.OfficialSource.updateOne({'starNotice' : temp_sub});
+                        result.OfficialSource.save();
+                    }
+                    else {
+                        res_data['status'] = 'failure';
+                        res_data['message'] = 'User has already starred the notice';
+                    }
+                }
+                else {
+                    log = `User could not be found with username ${username}`;
+                    res_data['status'] = 'failure';
+                    res_data['message'] = log;
+                    console.log(log);
+                }
+            }
+            else{
+                log = "Either the notice has been deleted or it doesn't exists";
+                res_data['status'] = 'failure';
+                res_data['message'] = log;
+                console.log(log);
+            }
+
+        }
+        else {
+            log = `The Official Source with username ${noticeSource} doesn't exists`;
+            res_data['status'] = 'failure';
+            res_data['message'] = log;
+            console.log(log);
+        }
+
+        res.json(res_data);
+        console.log(res_data);
+    });
+};
