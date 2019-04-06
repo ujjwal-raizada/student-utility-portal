29aa131581c15af1eeb9ef1d646b486fdc89c139
sat13mani <sat13mani@gmail.com>
2019-04-06 21:29:27 +0530

    frontend: Changed theme of ForgotPassword

--- front-end-react/frontend-testing/src/Components/ForgotPassword.js
+++ front-end-react/frontend-testing/src/Components/ForgotPassword.js
@@ -3,7 +3,8 @@ import Header from "./Header";
 
 class ForgotPassword extends Component {
   state = {
-    email: ""
+    email: "",
+    submitting: false
   };
 
   handleChange = event => {
@@ -12,36 +13,60 @@ class ForgotPassword extends Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  handlePassword = event => {
+    event.preventDefault();
+    this.setState({ submitting: true });
+    this.props.history.push(`/error/url`);
+    this.setState({ submitting: false });
+  };
+
   render() {
+    const spin = (
+      <span
+        class="spinner-border spinner-border-sm"
+        role="status"
+        aria-hidden="true"
+      />
+    );
     return (
-      <Fragment>
-        <Header page="ForgotPassword" />
-        <form class="form-horizontal" action="/action_page.php">
-          <div class="form-group">
-            <label class="control-label col-sm-4" for="email">
-              Email:
-            </label>
-            <div class="col-sm-4">
-              <input
-                type="email"
-                class="form-control"
-                id="email"
-                value={this.state.email}
-                name="email"
-                placeholder="Enter Email"
-                onChange={this.handleChange}
-              />
+      <div className="bg-light">
+        <Header page="forgotpassword" />
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+              <div className="card card-signin my-5">
+                <div className="card-body">
+                  <h5 className="card-title text-center">Forgot Password</h5>
+                  <form className="form-signin">
+                    <div className="form-label-group">
+                      <label for="inputEmail">Email address</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email address"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <br />
+
+                    <button
+                      className="btn btn-lg btn-primary btn-block"
+                      type="submit"
+                      onClick={this.handlePassword}
+                    >
+                      {this.state.submitting ? spin : ""} &nbsp;
+                      {this.state.submitting ? "Submitting" : "submit"}
+                    </button>
+                    <br />
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
-          <div class="form-group">
-            <div class="col-sm-offset-5 col-sm-4">
-              <button type="submit" class="btn btn-default">
-                Submit
-              </button>
-            </div>
-          </div>
-        </form>
-      </Fragment>
+        </div>
+      </div>
     );
   }
 }
