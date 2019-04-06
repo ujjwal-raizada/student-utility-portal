3f2353a6830881a647697de29e81c31284a90eb1
Prakhar Goenka <prakhar.goinka@gmail.com>
2019-04-06 22:09:57 +0530

    frontend: Show students and sources on admin page.

---
+++ front-end-react/frontend-testing/src/Components/SourcesList.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import axios from "axios";
+import config from "react-global-configuration";
+class SourcesList extends Component {
+  state = {
+    sources: []
+  };
+
+  componentDidMount() {
+    axios
+      .get(config.get("host_url") + config.get("routes.sources"))
+      .then(res => {
+        this.setState({
+          sources: res.data
+        });
+      });
+  }
+
+  render() {
+    const sources_list = this.state.sources.map((item, index) => (
+      <h6 key={index}>
+        {index + 1}
+        {" . "}
+        {item.username}
+      </h6>
+    ));
+
+    return (
+      <div>
+        <h2>Sources Registered</h2>
+        <br />
+        {sources_list}
+      </div>
+    );
+  }
+}
+
+export default SourcesList;
