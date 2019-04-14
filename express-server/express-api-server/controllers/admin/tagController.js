6063b71db5560ec626575942c2db0d2394cb4d39
Daksh Yashlaha <dyashlaha039909@gmail.com>
2019-04-14 14:12:02 +0530

    Express: Bug Fixes in Tags

--- express-server/express-api-server/controllers/admin/tagController.js
+++ express-server/express-api-server/controllers/admin/tagController.js
@@ -5,6 +5,10 @@ var async = require('async');
 exports.addtag = function(req, res) {
   var username = req.body.username;
   var tag = req.body.tag;
+  var response = {
+    'status' : 'failure',
+    'message' : 'processing'
+  }
 
   console.log('Adding tag: ' + tag);
 
@@ -14,10 +18,18 @@ exports.addtag = function(req, res) {
       .exec(callback);
     }
   }, function(err, result) {
-    if(err) next(err);
+    if(err) {
+      console.log('Error in Admin.findOne');
+      response['status'] = 'failure';
+      response['message'] = 'error';
+      return res.json(response);
+    };
 
     if(result.Admin == null){
       console.log("Insufficient permission for " + username);
+      response['status'] = 'failure';
+      response['message'] = 'insufficient permission';
+      return res.json(response);
     }
     else{
      async.parallel({
@@ -26,27 +38,46 @@ exports.addtag = function(req, res) {
         .exec(callback);
         }
       }, function(err, result) {
-        if(err) next(err);
+        if(err) {
+          console.log('Error in tag.findOne');
+          response['status'] = 'failure';
+          response['message'] = 'error';
+          return res.json(response);
+        };
 
         if(result.Tag == null){
           Tag.create({'name' : tag}, 
             function(err, instance) {
-              if(err) next(err);
+              if(err){
+                console.log('Error in Tag.create');
+                response['status'] = 'failure';
+                response['message'] = 'error';
+                return res.json(response);
+              };
             });
           console.log(tag + " has been added successfully");
+          response['status'] = 'success';
+          response['message'] = 'tag added successfully';
+          return res.json(response);
         }
         else {
           console.log(" The tag: " + tag + " already exists");
+          response['status'] = 'failure';
+          response['message'] = 'tag already exists';
+          return res.json(response);
         }
       });
     }
   });
-  res.json({'status': 'success'});
 };
 
 exports.deletetag = function(req, res) {
   var username = req.body.username;
   var tag = req.body.tag;
+  var response = {
+    'status' : 'failure',
+    'message' : 'processing'
+  };
 
   console.log('Deleting tag ' + tag);
 
@@ -56,11 +87,20 @@ exports.deletetag = function(req, res) {
       .exec(callback);
     }
   }, function(err, result) {
-      if(err) next(err);
+      if(err){
+        console.log('Error in Admin.findOne');
+        response['status'] = 'failure';
+        response['message'] = 'error';
+        return res.json(response);
+      };
 
       if(result.Admin == null) {
         console.log("Insufficient permission for " + username);
+        response['status'] = 'failure';
+        response['message'] = 'insufficient permission';
+        return res.json(response);
       }
+
       else{
         async.parallel({
           'Tag' : function(callback) {
@@ -68,20 +108,35 @@ exports.deletetag = function(req, res) {
             .exec(callback);
           }
         }, function(err, result) {
-            if(err) next(err);
+            if(err){
+              console.log('Error in Tag.findOne');
+              response['status'] = 'failure';
+              response['message'] = 'error';
+              return res.json(response);
+            };
 
             if(result.Tag == null) {
               console.log('Cannot delete tag: ' + tag  + ' It does not exist');
+              response['status'] = 'failure';
+              response['message'] = 'tag does not exist';
+              return res.json(response);
             }
             else{
               Tag.deleteOne({'name' : tag}, 
                 function(err, instance) {
-                  if(err) next(err);
+                  if(err){
+                    console.log('Error in Tag.deleteOne');
+                    response['status'] = 'failure';
+                    response['message'] = 'error';
+                    return res.json(response);
+                  };
                 });
               console.log('Tag: ' + tag + 'has been successfully removed');
+              response['status'] = 'success';
+              response['message'] = 'tag has been removed';
+              return res.json(response);
             }
         });
       }
   }); 
-  res.json({'status' : 'success'});
 };
